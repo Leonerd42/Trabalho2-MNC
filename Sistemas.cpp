292ca4f735dd5a14b87fd33a0d3a10b4efc863ff
Leonardo Oliveira <leosoliveira10@gmail.com>
2018-05-19 12:15:35 -0300

    calculo da determinante funcionando

--- Sistemas.cpp
+++ Sistemas.cpp
@@ -11,22 +11,74 @@
 
 #define max 10
 
+double Determinante (int ordem, double matriz[][max]); 
 
-int menu(){
+int Menu(){
 	
 	int x; 
 	
 	printf("\nEscolha a opção: "); 
-	printf("\n\n1 - blabla "); 
+	printf("\n\n1 - Cálculo de Determinante"); 
 	printf("\n10 - Fechar programa"); 
 	printf("\n\nOpção: "); 
+	fflush(stdin); 
 	scanf("%d",&x); 
 	
 	return x; 
 }
 
-void MostraMatriz ( int ordem, double matriz[][max]){
+void MenorPrincipal (double matriz[max][max],double Menor[max][max], int linha, int coluna, int ordem){
 	
+	int aux_l = 0, aux_c = 0; 
+	
+	for (int i=0; i<ordem; i++){
+		if(i == linha) continue; 
+		for (int j=0; j<ordem; j++){
+			if(j == coluna)	continue; 
+			Menor[aux_l][aux_c++] = matriz[i][j]; 			
+		}
+		aux_c = 0; 
+		aux_l++; 
+	}	
+}
+
+void MelhorCaminho (double matriz[max][max],int ordem, int *lin_col, int *n){
+	
+	int cont_i, cont_j, melh_i, melh_j, lin[ordem], col[ordem]; 
+	cont_i = cont_j = melh_i = melh_j = (*lin_col) = 0;	 
+	
+	for (int i=0; i<ordem; i++){
+		for(int j=0; j<ordem; j++)
+			if(matriz[i][j] == 0) cont_i++; 
+		lin[i] = cont_i; 
+		cont_i = 0; }
+	
+	for(int i=0; i<ordem; i++){
+		for(int j=0; j<ordem; j++)
+			if(matriz[j][i] == 0) cont_j++;
+		col[i] = cont_j; 
+		cont_j = 0; }
+	
+	for(int i=0; i<ordem; i++){
+		if(lin[i] != 0)
+			if(lin[melh_i] < lin[i])
+				melh_i = i; }
+	
+	for(int i=0; i<ordem; i++){
+		if(col[i] != 0)
+			if(col[melh_j] < col[i])
+				melh_j = i; }
+	
+	if(lin[melh_i] >= col[melh_j]){
+		(*lin_col) = 0; 
+		(*n) = melh_i; 
+	}else{
+		(*lin_col) = 1; 
+		(*n) = melh_j; 
+	}	
+}
+
+void MostraMatriz ( int ordem, double matriz[][max]){	
 	for(int i=0; i<ordem; i++){
 		printf("\n"); 
 		for(int j=0; j<ordem; j++)
@@ -40,67 +92,68 @@ double Cofator (int ordem, int i, int j, double matriz[][max]){
 
 double Determinante (int ordem, double matriz[][max]){
 	
-	int i, j; 
+	int i, j, lin_col, num;
 	double maux [max][max]; 
-	double soma = 0; 
+	int soma = 0; 
 	
-	if(ordem == 2){
-		return (matriz[0][0] * matriz[1][1] - matriz[0][1]*matriz[1][0]);		
-	}
+	if(ordem == 2) return (matriz[0][0] * matriz[1][1] - matriz[0][1]*matriz[1][0]);		
 	
-	if(ordem == 1){
-		return matriz[0][0];
-	}
+	if(ordem == 1) return matriz[0][0];
 	
-	//linha se mantem fixa 
-	i = 0; 
-		
-	for(j=0; j<ordem; j++){
-		if(j == ordem-1){
-			
-			
-		}else{
-		
-			MenorPrincipal(matriz,maux,i,j,ordem); 
-			
-		}
-		
-		//return 
-		
-		
-		//Calcula menorprincipal de Aij
-		// um valor * ( -1^i+j * menor principal) 
-	}
-		
+	MelhorCaminho(matriz,ordem,&lin_col,&num);
 	
+	switch(lin_col){
+		case 0: 	i = num; 
+					for(j=0; j<ordem; j++){
+					
+						MenorPrincipal (matriz,maux,i,j,ordem); 
+						soma += matriz[i][j] * Cofator (ordem,i,j,maux);
+					}
+					break; 
+		case 1: 	j = num; 
+					for(i=0; i<ordem; i++){
+					
+						MenorPrincipal (matriz,maux,i,j,ordem); 
+						soma += matriz[i][j] * Cofator (ordem,i,j,maux);
+					}
+					break; 	}
+	
+	return soma; 
 }
 
-void MenorPrincipal (double matriz[max][max],double Menor[max][max], int linha, int coluna, int ordem){
+LeMatriz(double matriz[][max], int *ordem){
 	
-	int aux_l = 0; 
-	int aux_c = 0; 
+	printf("\nDigite a ordem da matriz: "); 
+	scanf("%d",&(*ordem)); 
+	printf("\nDigite a matriz: "); 
+	for(int i=0; i<(*ordem); i++)
+		for (int j=0; j<(*ordem); j++)
+			scanf("%lf",&matriz[i][j]); 
 	
-	for (int i=0; i<ordem; i++){
-		if(i == linha)
-			continue; 
-		for (int j=0; j<ordem; j++){
-			if(j == coluna)
-				continue; 
-			Menor[aux_l][aux_c++] = matriz[i][j]; 			
-		}
-		aux_c = 0; 
-		aux_l++; 
-	}	
 }
 
 int main(){
 
 	setlocale(LC_ALL,"Portuguese");
-	int ordem; 
-	double matriz[max][max];	
-	double menor[max][max]; 
+	int ordem, det, aux = 0, num, op; 
+	double matriz[max][max], menor[max][max]; 
 	
-	freopen("matriz.txt","r",stdin); 
+	//freopen("matriz.txt","r",stdin); 
+	
+	do{
+		op = Menu(); 
+		switch(op){
+			case 1: 	LeMatriz(matriz,&ordem); 
+						printf("\nMatriz: \n"); 
+						MostraMatriz(ordem, matriz); 
+						printf("\n\nDet(A) = %d",Determinante(ordem,matriz));
+						fflush(stdin); 
+						system("pause"); 
+						break; 
+			case 2: 	
+						break; 
+		}
+	}while(true); 
 	
 	scanf("%d",&ordem); 
 	for(int i=0; i<ordem; i++)
@@ -111,11 +164,18 @@ int main(){
 	printf("\nOrdem: %d",ordem); 
 	MostraMatriz(ordem,matriz); 
 	
-	printf("\nMenor Principal: \n"); 
-	MenorPrincipal(matriz,menor,0,2,ordem);
-	MostraMatriz(ordem-1,menor);
+	/*printf("\nMelhor caminho para calcular a determinante: "); 
+	MelhorCaminho(matriz,ordem,&aux,&num); 
 	
-	//printf("\n\nValor teste: %lf",Cofator(1,2,2,matriz)); 
+	if(!aux)
+		printf("\nLinha de numero %d",num+1); 
+	else printf("\nColuna de numero %d",num+1); 
+	
+	printf("\n\n");*/
+	
+	det = Determinante(ordem,matriz); 
+	
+	printf("\nDeterminante: %d",det); 
 	
 	printf("\nFim do programa"); 
 }
--- T2.cpp
+++ T2.cpp
@@ -1,60 +1,115 @@
-/*
-Alunos: 
+/***************************************************
+		Trabalho de MNC - Sistemas Lineares
 Lucas Henrique Russo do Nascimento, RA: 171025709
 Bruna Lika Tamake, RA: 171024427
 Barbara Este Fernandez, RA:161025901
 Leonardo Silva de Oliveira, RA: 171025903
-*/
+
+*****************************************************/
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
 #include<ctype.h>
 #include<math.h>
-#define MAX 10
-int n;
-//------------------------------------------
-//Determinante:
-float **ReduzMatriz(int ordem, int i, float **matriz, float **matrizReduzida){
-	/*int j, k;
-	float **p=matrizReduzida, *p2;
-	for(j = 1; j < ordem; j++){
-		p2 = *p++;
-		for(k = 0; k < ordem; k++){
-			if(j==i){
-				continue;
-			}
-			else{
-				*p2++ = matriz[j][k];
-			}
+#define max 10
+
+double Determinante (int ordem, double matriz[][max]);
+
+//  CALCULO DE DETERMINANTE
+
+void MenorPrincipal (double matriz[max][max],double Menor[max][max], int linha, int coluna, int ordem){
+	
+	int aux_l = 0, aux_c = 0; 
+	
+	for (int i=0; i<ordem; i++){
+		if(i == linha) continue; 
+		for (int j=0; j<ordem; j++){
+			if(j == coluna)	continue; 
+			Menor[aux_l][aux_c++] = matriz[i][j]; 			
 		}
-	}
-	return matrizReduzida;*/
+		aux_c = 0; 
+		aux_l++; 
+	}	
 }
-float Determinante(int ordem, float **matriz){
-	/*float det=0;
-	if(ordem==1){
-		det=matriz[1][1];
-		return det;
-	}
-	int cofator;
-	float **matrizReduzida;
-	for(int i = 0; i < ordem; i++){
-		if(i%2==1){
-			ReduzMatriz(ordem, i, matriz, matrizReduzida);
-			cofator=(-1)*Determinante(ordem-1, matrizReduzida);
-		}
-		else{
-			ReduzMatriz(ordem, i, matriz, matrizReduzida);
-			cofator=1*Determinante(ordem-1, matrizReduzida);
-		}
-		det += matriz[0][i]*cofator;
-	}*/
+
+void MelhorCaminho (double matriz[max][max],int ordem, int *lin_col, int *n){
+	
+	int cont_i, cont_j, melh_i, melh_j, lin[ordem], col[ordem]; 
+	cont_i = cont_j = melh_i = melh_j = (*lin_col) = 0;	 
+	
+	for (int i=0; i<ordem; i++){
+		for(int j=0; j<ordem; j++)
+			if(matriz[i][j] == 0) cont_i++; 
+		lin[i] = cont_i; 
+		cont_i = 0; }
+	
+	for(int i=0; i<ordem; i++){
+		for(int j=0; j<ordem; j++)
+			if(matriz[j][i] == 0) cont_j++;
+		col[i] = cont_j; 
+		cont_j = 0; }
+	
+	for(int i=0; i<ordem; i++){
+		if(lin[i] != 0)
+			if(lin[melh_i] < lin[i])
+				melh_i = i; }
+	
+	for(int i=0; i<ordem; i++){
+		if(col[i] != 0)
+			if(col[melh_j] < col[i])
+				melh_j = i; }
+	
+	if(lin[melh_i] >= col[melh_j]){
+		(*lin_col) = 0; 
+		(*n) = melh_i; 
+	}else{
+		(*lin_col) = 1; 
+		(*n) = melh_j; 
+	}	
 }
-//------------------------------------------
-int menu_metodos(){
-	int op;
+
+double Cofator (int ordem, int i, int j, double matriz[][max]){
+	return pow((-1),i+j) * Determinante(ordem-1,matriz);  
+}
+
+double Determinante (int ordem, double matriz[][max]){
+	
+	int i, j, lin_col, num;
+	double maux [max][max]; 
+	int soma = 0; 
+	
+	if(ordem == 2) return (matriz[0][0] * matriz[1][1] - matriz[0][1]*matriz[1][0]);		
+	
+	if(ordem == 1) return matriz[0][0];
+	
+	MelhorCaminho(matriz,ordem,&lin_col,&num);
+	
+	switch(lin_col){
+		case 0: 	i = num; 
+					for(j=0; j<ordem; j++){
+					
+						MenorPrincipal (matriz,maux,i,j,ordem); 
+						soma += matriz[i][j] * Cofator (ordem,i,j,maux);
+					}
+					break; 
+		case 1: 	j = num; 
+					for(i=0; i<ordem; i++){
+					
+						MenorPrincipal (matriz,maux,i,j,ordem); 
+						soma += matriz[i][j] * Cofator (ordem,i,j,maux);
+					}
+					break; 	}
+	
+	return soma; 
+}
+
+//  FIM DAS ROTINAS DO CALCULO DE DETERMINANTE
+
+int MenuMetodos(){
+	int op = 0;
 	do{
 		system("cls");
+		fflush(stdin);
 		printf("\n\tMENU\t\n");
 		printf("\n\t1-Determinante \n\t2-Sistema Triangular Inferior \n\t3-Sistema Triangular Superior \n\t4-Decomposicao LU \n\t5-Cholesky \n\t6-Gauss Compacto \n\t7-Gauss Jordan \n\t8-GPP SEM troca de linhas \n\t9-Jacobi \n\t10-Gauss Seidel \n\t11-Matriz Inversa\n\t12-Fechar programa\n");
 		printf("\n\tDigite a opcao:");
@@ -66,23 +121,19 @@ int menu_metodos(){
 	return op;
 }
 //------------------------------------------
-void ColetaDados(int *ordem, int opcao){
+void ColetaDados(int *ordem, int opcao, double matriz[][max]){
 	if(opcao==1){ // coleta dados para calculo do determinante
 		system("cls");
 		printf("\n\tDigite a ordem da matriz:");
-		scanf("%d", &*ordem);
+		scanf("%d", &(*ordem));
+		printf("\nDigite a Matriz: \n"); 
+		for(int i=0; i<(*ordem); i++)
+			for(int j=0; j<(*ordem); j++)
+				scanf("%lf",&matriz[i][j]);
 	}
 }
-//------------------------------------------
-void LerMatriz(float **matriz, int ordem){
-	for(int i=0; i<ordem; i++){
-		for(int j=0; j<ordem; j++){
-			printf("a%d%d = ",i+1, j+1);
-			scanf("%f", &matriz[i][j]);
-		}
-	}
-}
-void MostraMatriz(float **matriz, int ordem){
+
+void MostraMatriz(double matriz[max][max], int ordem){
 	printf("\n\tMatriz:\n");
 	for(int i=0; i<ordem; i++){
 		for(int j=0; j<ordem; j++){
@@ -91,52 +142,20 @@ void MostraMatriz(float **matriz, int ordem){
 		printf("\n");
 	}
 }
-float **alocarMatriz(int ordem){
-	float **m;
-	m = (float**)calloc(ordem, sizeof(float*));
-	if(m == NULL){
-		exit(1);
-	}
-	for(int i=0; i<ordem; i++){
-		*(m+i) = (float*)calloc(n, sizeof(float));
-		if(*(m+i) == NULL){
-			exit(1);
-		}
-	}
-	return m;
-}
 //------------------------------------------
 int main(){
 	int opcao, ordem;
-	float det, **matriz; 
-	opcao=menu_metodos();
-	switch(opcao){
-		case 1:
-			ColetaDados(&ordem, 1);
-			matriz= alocarMatriz(ordem);
-			LerMatriz(matriz, ordem);
-			//MostraMatriz(matriz, ordem);
-			det=Determinante(ordem, matriz);
-			printf("det(A) = %f", det);
-			break;
-		/*case 2:
-			break;
-		case 3:
-			break;
-		case 4:
-			break;
-		case 5:
-			break;
-		case 6:
-			break;
-		case 7:
-			break;
-		case 8:
-			break;
-		case 9:
-			break;
-		default:
-			return 0;
-			break;*/
-	}
+	double det, matriz[max][max]; 	
+	//freopen("matriz.txt","r",stdin); 
+	do{
+		opcao = MenuMetodos();
+		switch(opcao){
+			case 1:		ColetaDados(&ordem, 1, matriz);
+						MostraMatriz(matriz,ordem);
+						det=Determinante(ordem, matriz); 
+						printf("det(A) = %lf\n\t", det);
+						system("pause"); 
+						break;
+		}
+	}while(opcao != 12);
 }
